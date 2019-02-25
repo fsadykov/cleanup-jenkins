8f23c824dec722c6e94a0abddeda41e772b98c8c
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 13:21:57 -0600

    Added stage

--- cleaner.groovy
+++ cleaner.groovy
@@ -1,15 +1,15 @@
 
 def diskUsage
 
-def checkForCleanUp(){
-  diskUsage = sh returnStdout: true, script: ''''df -h | grep fscoding | awk '{print $5 + ""}'''''
-}
+
+
 
 node('master'){
+  stage('checking for ') {
+    diskUsage = sh returnStdout: true, script: ''''df -h | grep fscoding | awk '{print $5 + ""}'''''
+  }
   if (diskUsage > 80) {
-    stage('checking for ') {
-      println('I will clean up ')
-    }
+    println('Starting the clean up')
   } else {
     println("The disk usage below 80% nothing to clean")
   }
