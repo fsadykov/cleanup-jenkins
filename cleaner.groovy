ed458f980c8551bddd4f266de8311fe4a2adaac6
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 15:09:34 -0600

    Added script to clean

--- cleaner.groovy
+++ cleaner.groovy
@@ -16,6 +16,7 @@ node('master'){
   if (buildCheker['size'].toInteger() > 10) {
     emailer()
     sh 'echo $HOME '
+    sh 'sh scripts/outOfSpaceJenkins.sh'
   } else {
     println("The disk usage below 80% nothing to clean")
   }
