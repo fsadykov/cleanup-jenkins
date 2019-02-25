a56be61162743410d2f38a36b7690e1ec680b6f7
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 14:14:37 -0600

    Checking the function

--- cleaner.groovy
+++ cleaner.groovy
@@ -1,13 +1,12 @@
 
+
+def buildCheker = [:]
+
 def emailer(){
   println(''' Hello everyon,
   Our jenins is out of space. Pleas take a look the jenkins server
   Thank you ''')
 }
-
-def buildCheker = [:]
-def commonEmail = emailer()
-
 
 
 node('master'){
@@ -15,7 +14,7 @@ node('master'){
   buildCheker['size'] = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
 
   if (buildCheker['size'].toInteger() > 10) {
-    commonEmail()
+    emailer()
   } else {
     println("The disk usage below 80% nothing to clean")
   }
