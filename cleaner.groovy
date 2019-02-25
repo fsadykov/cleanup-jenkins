08d6b05fe85bd1f6fee913731075b7d66d49c66f
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 14:13:02 -0600

    Created example emailer

--- cleaner.groovy
+++ cleaner.groovy
@@ -1,12 +1,19 @@
 
 def buildCheker = [:]
+def commonEmail = emailer()
+
+def emailer(){
+  println(''' Hello everyon,
+  Our jenins is out of space. Pleas take a look the jenkins server
+  Thank you ''')
+}
 
 node('master'){
   checkout scm
   buildCheker['size'] = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
 
-  if (buildCheker['size'].toInteger() > 80) {
-    println('Starting the clean up')
+  if (buildCheker['size'].toInteger() > 10) {
+    commonEmail()
   } else {
     println("The disk usage below 80% nothing to clean")
   }
