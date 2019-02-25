38eb5654a31e31d81d5a8c583a8cb850e273de7e
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 13:41:53 -0600

    Added if statement

--- cleaner.groovy
+++ cleaner.groovy
@@ -3,10 +3,9 @@ def diskUsage
 
 node('master'){
   diskUsage = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
-  println(diskUsage)
-  // if (diskUsage > 80) {
-  //   println('Starting the clean up')
-  // } else {
-  //   println("The disk usage below 80% nothing to clean")
-  // }
+  if (diskUsage > 80) {
+    println('Starting the clean up')
+  } else {
+    println("The disk usage below 80% nothing to clean")
+  }
 }
