4611f314422581495ea6114be51bffdb807287ff
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 13:44:09 -0600

    Checking object type

--- cleaner.groovy
+++ cleaner.groovy
@@ -3,6 +3,8 @@ int diskUsage
 
 node('master'){
   diskUsage = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
+
+  println(diskUsage.class)
   if (diskUsage > 80) {
     println('Starting the clean up')
   } else {
