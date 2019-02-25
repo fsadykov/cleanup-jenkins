8e88ac0afb6b4574372627a78872467f17103f5b
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 13:44:51 -0600

    Excluded if statement

--- cleaner.groovy
+++ cleaner.groovy
@@ -5,9 +5,9 @@ node('master'){
   diskUsage = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
 
   println(diskUsage.class)
-  if (diskUsage > 80) {
-    println('Starting the clean up')
-  } else {
-    println("The disk usage below 80% nothing to clean")
-  }
+  // if (diskUsage > 80) {
+  //   println('Starting the clean up')
+  // } else {
+  //   println("The disk usage below 80% nothing to clean")
+  // }
 }
