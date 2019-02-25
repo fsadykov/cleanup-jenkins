40497b2a006b26d3a55909e58a80d0074efddb71
farkhodsadykov <sadykovfarkhod@gmail.com>
2019-02-25 13:53:21 -0600

    Diskusage changed to map

--- cleaner.groovy
+++ cleaner.groovy
@@ -1,14 +1,13 @@
 
-int diskUsage
+def buildCheker = [:]
 
 node('master'){
   checkout scm
-  diskUsage = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
-  println(diskUsage)
-  println(diskUsage.class)
-  // if (diskUsage > 80) {
-  //   println('Starting the clean up')
-  // } else {
-  //   println("The disk usage below 80% nothing to clean")
-  // }
+  buildCheker['size'] = sh(returnStdout: true, script: '''df -h | grep fscoding |awk '{print $5 + ""}' ''' )
+
+  if (buildCheker['size'].toInteger() > 80) {
+    println('Starting the clean up')
+  } else {
+    println("The disk usage below 80% nothing to clean")
+  }
 }
